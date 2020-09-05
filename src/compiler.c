dfa5ea4253852457acfdd0c7c7f5104ae99b79e3
mistersouls <soulsmister@gmail.com>
2020-09-05 10:45:15 +0200

    Tuto 18: Implement visit functions for compiler

--- src/compiler.c
+++ src/compiler.c
@@ -10,15 +10,6 @@ Bytecode *compile(AST *ast, Stackframe *sf) {
     void *argv[] = {&bytecode, &sf};
     cvisit_ast(self, ast, 2, argv);
 
-    /** This a test*/
-    push(&bytecode, ILOAD_VAR, 0, true);
-    push(&bytecode, ILOAD_CONST, 1, true);
-    push(&bytecode, ISTORE, 0, true);
-    push(&bytecode, ILOAD_VAR, 0, true);
-    push(&bytecode, IECHO, -1, false);
-
-    /* end test */
-
     return bytecode;
 }
 
@@ -85,34 +76,184 @@ static void reverse(Bytecode **bytecode) {
 }
        
 void cvisit_ast(Visitor self, AST *ast, uint8_t argc, void **argv) {
+    while (ast != NULL) {
+	switch (ast->type) {
+	    case INSTRUCTION_DECLARATION:
+		ast
+		 ->declaration
+		 ->node
+		 ->accept(ast->declaration, self, argc, argv);
+		break;
+	    case INSTRUCTION_ASSIGNMENT:
+		ast
+		 ->assignment
+		 ->node
+		 ->accept(ast->assignment, self, argc, argv);
+		break;
+	    case INSTRUCTION_BFC:
+		ast
+		 ->bfc
+		 ->node
+		 ->accept(ast->bfc, self, argc, argv);
+		break;
+	    default:
+		printf("Internal compilation error (ICE): this statement is not supported.\n");
+		exit(1);
+	}
 
+	ast = ast->next;
+    }
 }
 
 void cvisit_declaration(Visitor self, Declaration *declaration, uint8_t argc, void **argv) {
+    if (argc < 2) {
+	printf("Asserrtion error: argument count must be greater than 1.\n");
+	exit(1);
+    }
 
+    bool stored = false;
+    void *new[] = {argv[0], argv[1], &stored};
+
+    declaration
+	->id
+	->node
+	->accept(declaration->id, self, 3, new);
 }
 
 void cvisit_assignment(Visitor self, Assignment *assignment, uint8_t argc, void **argv) {
+    if (argc < 2) {
+	printf("Asserrtion error: argument count must be greater than 1.\n");
+	exit(1);
+    }
 
+    assignment
+	->expression
+	->node
+	->accept(assignment->expression, self, argc, argv);
+    
+    bool stored = true;
+    void *new[] = {argv[0], argv[1], &stored};
+
+	
+    assignment
+	->id
+	->node
+	->accept(assignment->id, self, 3, new);
 }
 
 void cvisit_bfc(Visitor self, BuiltinFuncCall *bfc, uint8_t argc, void **argv) {
+    if (argc < 1) {
+	printf("Asserrtion error: argument count must be greater than 0.\n");
+	exit(1);
+    }
 
+    bfc 
+     ->expression
+     ->node
+     ->accept(bfc->expression, self, argc, argv);
+
+    Bytecode **bytecode = argv[0];
+    Bytecode *current = *bytecode;
+
+    if (current == NULL) {
+	printf("Internal compilation error: unexpected error.\n");
+	exit(1);
+    }
+
+    switch (bfc->type) {
+	case BUILTIN_PRINT: {
+	    if (current->opcode == ILOAD_VAR || current->opcode == ILOAD_CONST) {
+		push(bytecode, IECHO, -1, false);
+	    } else {
+		printf("Internal compilation error: this statement is not supported.\n");
+		exit(1);
+	    }
+	    break;
+	}
+	default:
+	    printf("Internal compilation error: this statement is not supported.\n");
+	    exit(1);
+    }
 }
 
 void cvisit_expression(Visitor self, Expression *expression, uint8_t argc, void **argv) {
+    if (argc < 2) {
+	printf("Asserrtion error: argument count must be greater than 1.\n");
+	exit(1);
+    }
+
+    bool stored = false;
+    void *new[] = {argv[0], argv[1], &stored};
+
+    switch (expression->type) {
+	case EXPRESSION_NUMBER:
+	    expression
+		->number
+		->node
+		->accept(expression->number, self, argc, argv);
+	    break;
+	case EXPRESSION_ID:
+	    expression
+		->id
+		->node
+		->accept(expression->id, self, 3, new);
+	    break;
+	default:
+	    printf("Internal compilation error: this statement is not supported.\n");
+	    exit(1);
+    }
 
 }
 
 void cvisit_type(Visitor self, Type *type, uint8_t argc, void **argv) {
-
+    /* Nothing to do */
 }
 
 void cvisit_id(Visitor self, Id *id, uint8_t argc, void **argv) {
+    if (argc < 3) {
+	printf("Asserrtion error: argument count must be greater than 2.\n");
+	exit(1);
+    }
+
+    Bytecode **bytecode = argv[0];
+    Stackframe **sf = argv[1];
+    bool *stored = argv[2];
+    Dataframe *ref = find_data(*sf, id->value);
+
+    if (ref != NULL) {
+	switch (ref->type) {
+	    case TYPE_NUMBER: {
+		Opcode opcode = *stored ? ISTORE : ILOAD_VAR;
+		push(bytecode, opcode, ref->addr, true);
+		break;
+	    }
+	    default:
+		printf("Internal compilation error: this statement is not supported.\n");
+		exit(1);
+	}
+    } else {
+	printf("Internal compilation error: unable to process id %s.\n", id->value);
+	exit(1);
+    }
 
 }
 
 void cvisit_number(Visitor self, Number *number, uint8_t argc, void **argv) {
+    if (argc < 1) {
+	printf("Asserrtion error: argument count must be greater than 0.\n");
+	exit(1);
+    }
 
+    Bytecode **bytecode = argv[0];
+
+    switch (number->type) {
+	case NUMBER_INTEGER:
+	    push(bytecode, ILOAD_CONST, number->integer, true);
+	    break;
+	default:
+	    printf("Internal compilation error: this statement is not supported.\n");
+	    exit(1);
+
+    }
 }
 
